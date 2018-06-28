18ab365850d705acbc6bbab78eccd86b6d44648c
Priyanshu Mehta <mehta7priyanshu@gmail.com>
2018-06-28 22:49:58 +0530

    Add files via upload

---
+++ SimpleLinearRegression.R
@@ -0,0 +1,47 @@
+
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+#Fitting Simple Linear Regression to the Training Set
+
+regressor = lm (formula = Salary ~ YearsExperience , 
+                data = training_set)
+
+#Predicting the test set results
+
+y_pred = predict(regressor, newdata = test_set)
+
+#Visualising Results in graph
+
+install.packages('ggplot2')
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = training_set$YearsExperience , y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes( x = training_set$YearsExperience, predict(regressor, newdata = training_set) ) , 
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Training Set)') +
+  xlab ('Years of Experience') +
+  ylab ('Salary')
+
+#test set results
+ggplot() + 
+  geom_point(aes(x = test_set$YearsExperience , y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes( x = training_set$YearsExperience, predict(regressor, newdata = training_set) ) , 
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Test Set)') +
+  xlab ('Years of Experience') +
+  ylab ('Salary')
